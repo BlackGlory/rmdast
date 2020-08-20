1f6f9050a72c22eb8e362fb0b7d2513bc4e21422
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-08-20 13:59:38 +0800

    feat: add remark-footnotes

--- src/parse.ts
+++ src/parse.ts
@@ -1,10 +1,14 @@
-import { Root } from 'mdast'
+import * as MDAST from 'mdast'
+import * as AST from './ast'
 import unified = require('unified')
 import markdown = require('remark-parse')
+import footnotes = require('remark-footnotes')
+import { transform } from './transform'
 
-export function parse(text: string): Root {
+export function parse(text: string): AST.Root {
   const node = unified()
     .use(markdown)
-    .parse(text) as Root
-  return node
+    .use(footnotes, { inlineNotes: true })
+    .parse(text) as MDAST.Root
+  return transform(node)
 }
---
+++ src/remark-footnotes.d.ts
@@ -0,0 +1,4 @@
+declare module 'remark-footnotes' {
+  const parser: any
+  export = parser
+}
