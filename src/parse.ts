61c8f12e607c8eb45e8a822ff288de7d90b0ff65
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-08-25 10:31:56 +0800

    feat: add plugins parameter

--- src/parse.ts
+++ src/parse.ts
@@ -4,11 +4,22 @@ import markdown = require('remark-parse')
 import footnotes = require('remark-footnotes')
 import { html } from './remark-html'
 
-export function parse(text: string): MDAST.Root {
-  const node = unified()
+export function parse(
+  text: string
+, plugins: Array<Parameters<unified.Processor<unified.Settings>['use']>> = []
+): MDAST.Root {
+  let processor = unified()
     .use(markdown)
+
+  // The plugins for rmdast should not create new ast node types.
+  // They should convert new things to html, so rmdast can parse them as components.
+  for (const plugin of plugins) {
+    processor = processor.use(...plugin)
+  }
+
+  processor = processor
     .use(footnotes, { inlineNotes: true })
     .use(html)
-    .parse(text) as MDAST.Root
-  return node
+
+  return processor.parse(text) as MDAST.Root
 }
