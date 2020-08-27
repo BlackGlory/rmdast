73c5c110a7986c334265daf81e55293795ebca5f
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-08-27 17:54:45 +0800

    feat: passing the original node to fn

--- src/rmdast-utils/flat-map.ts
+++ src/rmdast-utils/flat-map.ts
@@ -1,13 +1,13 @@
 import * as AST from '@src/ast'
 import { isParent, isComponent } from '@src/is'
-import { produce } from 'immer'
+import { produce, original } from 'immer'
 import 'core-js/es/array/flat-map'
 
 export function flatMap(node: AST.Node, fn: (node: AST.Node) => AST.Node[]): AST.Node[] {
   const newNodes = fn(node)
   return newNodes.map(node => produce(node, node => {
     if (isParent(node) && !isComponent(node)) {
-      node.children = node.children.flatMap(x => flatMap(x, fn))
+      node.children = original(node)!.children.flatMap(x => flatMap(x, fn))
     }
   }))
 }
--- src/rmdast-utils/map.ts
+++ src/rmdast-utils/map.ts
@@ -1,12 +1,12 @@
 import * as AST from '@src/ast'
 import { isParent, isComponent } from '@src/is'
-import { produce } from 'immer'
+import { produce, original } from 'immer'
 
 export function map(node: AST.Node, fn: (node: AST.Node) => AST.Node): AST.Node {
   const newNode = fn(node)
   return produce(newNode, node => {
     if (isParent(node) && !isComponent(node)) {
-      node.children = node.children.map(x => map(x, fn))
+      node.children = original(node)!.children.map(x => map(x, fn))
     }
   })
 }
