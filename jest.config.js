302ca64ac3b5c184bb7aad30e13fa5ab5684e683
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-07-17 19:10:24 +0000

    Revert "feat: micromark-extension-component(buggy)"
    This reverts commit 95274ad58566b8c8cb99425fb18f563f83c48e97.

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,10 @@
 const { pathsToModuleNameMapper } = require('ts-jest/utils')
 const { compilerOptions } = require('./tsconfig.base.json')
 
-const esModules = [
-  'micromark-util-symbol'
-, 'micromark-util-character'
-, 'micromark-util-html-tag-name'
-, 'micromark-factory-space'
-, 'mdast-util-from-markdown'
-, 'mdast-util-to-string'
-, 'micromark'
-, 'parse-entities'
-, 'character-entities'
-, 'unist-util-stringify-position'
-].join('|')
-
 module.exports = {
   preset: 'ts-jest'
 , testEnvironment: 'node'
 , testMatch: ['**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)']
-, transform: {
-    '^.+\\.ts$': 'ts-jest'
-  , [`(${esModules}).+\\.js$`]: 'babel-jest'
-  }
-, transformIgnorePatterns: [`/node_modules/(?!${esModules})`]
 , moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/'
   })
--- src/micromark-extension-component/README.md
+++
@@ -1,3 +0,0 @@
-# micromark-extension-component
-
-经过实验, 在micromark上重现remark-parse v8那样的解析修改是不可能的.
