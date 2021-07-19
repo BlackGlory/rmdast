a79a451de37d0b48b69ae5f0a5bbdd9fb925f2be
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-07-19 14:32:39 +0000

    refactor: remove core-js

--- src/rmdast-utils/flat-map.ts
+++ src/rmdast-utils/flat-map.ts
@@ -1,13 +1,19 @@
 import * as RMDAST from './rmdast-2.0'
 import { isParent } from './is'
 import { produce, original } from 'immer'
-import 'core-js/es/array/flat-map'
 
-export function flatMap(node: RMDAST.Node, fn: (node: RMDAST.Node) => RMDAST.Node[]): RMDAST.Node[] {
+export function flatMap(
+  node: RMDAST.Node
+, fn: (node: RMDAST.Node) => RMDAST.Node[]
+): RMDAST.Node[] {
   const newNodes = fn(node)
   return newNodes.map(node => produce(node, node => {
     if (isParent(node)) {
-      node.children = node.children.flatMap(x => flatMap(original(x)!, fn))
+      const newChildNodes: RMDAST.Node[] = []
+      for (const childNode of node.children) {
+        newChildNodes.push(...flatMap(original(childNode)!, fn))
+      }
+      node.children = newChildNodes
     }
   }))
 }
