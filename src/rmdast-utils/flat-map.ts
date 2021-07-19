c70cf938c89c4b96d28e2afd6feafc2be46b9635
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-07-19 18:39:44 +0000

    refactor: remove iterable-opeartor

--- src/rmdast-utils/flat-map.ts
+++ src/rmdast-utils/flat-map.ts
@@ -1,7 +1,6 @@
 import * as RMDAST from './rmdast-2.0'
 import { isParent } from './is'
 import { produce, original } from 'immer'
-import { map, flatten, toArray } from 'iterable-operator'
 
 export function flatMap(
   node: RMDAST.Node
@@ -10,14 +9,7 @@ export function flatMap(
   const newNodes = fn(node)
   return newNodes.map(node => produce(node, node => {
     if (isParent(node)) {
-      node.children = toArray(
-        flatten(
-          map(
-            node.children
-          , node => flatMap(original(node)!, fn)
-          )
-        )
-      )
+      node.children = node.children.map(node => flatMap(original(node)!, fn)).flat()
     }
   }))
 }
