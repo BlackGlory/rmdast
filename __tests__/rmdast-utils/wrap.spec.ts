f3de1c8e4a168170a25355bf597471a63e3db8e6
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-08-10 17:02:31 +0000

    feat: add wrapAST

---
+++ __tests__/rmdast-utils/wrap.spec.ts
@@ -0,0 +1,108 @@
+import { wrapAST, WrappedNode } from '@src/rmdast-utils/wrap'
+import * as R from '@src/rmdast-utils/builder'
+
+describe('wrapAST', () => {
+  test('root', () => {
+    const ast = R.root([
+      R.paragraph([
+        R.text('first')
+      ])
+    , R.paragraph([
+        R.text('middle')
+      ])
+    , R.paragraph([
+        R.text('last')
+      ])
+    ])
+
+    const result = wrapAST(ast)
+
+    expect(result).toMatchObject({
+      type: 'root'
+    , parent: null
+    , previousSibling: null
+    , nextSibling: null
+    , children: [
+        {
+          type: 'paragraph'
+        , previousSibling: null
+        , children: [
+            {
+              type: 'text'
+            , value: 'first'
+            , previousSibling: null
+            , nextSibling: null
+            }
+          ]
+        }
+      , {
+          type: 'paragraph'
+        , children: [
+            {
+              type: 'text'
+            , value: 'middle'
+            , previousSibling: null
+            , nextSibling: null
+            }
+          ]
+        }
+      , {
+          type: 'paragraph'
+        , nextSibling: null
+        , children: [
+            {
+              type: 'text'
+            , value: 'last'
+            , previousSibling: null
+            , nextSibling: null
+            }
+          ]
+        }
+      ]
+    })
+    expect(result.children[0].nextSibling).toBe(result.children[1])
+    expect(result.children[0].parent).toBe(result)
+    expect((result.children[0] as any).children[0].parent).toBe(result.children[0])
+    expect(result.children[1].parent).toBe(result)
+    expect(result.children[1].previousSibling).toBe(result.children[0])
+    expect(result.children[1].nextSibling).toBe(result.children[2])
+    expect((result.children[1] as any).children[0].parent).toBe(result.children[1])
+    expect(result.children[2].parent).toBe(result)
+    expect(result.children[2].previousSibling).toBe(result.children[1])
+    expect((result.children[2] as any).children[0].parent).toBe(result.children[2])
+  })
+
+  test('table.header', () => {
+    const ast = R.root([
+      R.table(
+        R.tableRow([])
+      , []
+      )
+    ])
+
+    const result = wrapAST(ast)
+
+    expect(result).toMatchObject({
+      type: 'root'
+    , parent: null
+    , previousSibling: null
+    , nextSibling: null
+    , children: [
+        {
+          type: 'table'
+        , previousSibling: null
+        , nextSibling: null
+        , header: {
+            type: 'tableRow'
+          , children: []
+          , previousSibling: null
+          , nextSibling: null
+          }
+        , children: []
+        }
+      ]
+    })
+    expect(result.children[0].parent).toBe(result)
+    expect((result.children[0] as any).header.parent).toBe(result.children[0])
+  })
+})
---
+++ src/rmdast-utils/wrap.ts
@@ -0,0 +1,198 @@
+import * as AST from './rmdast-2.0'
+import { Mixin } from 'hotypes'
+import { isParent, isTable } from './is'
+import cloneDeep from 'lodash.clonedeep'
+import { isntUndefined } from '@blackglory/types'
+
+export type WrappedNode<
+  Node extends AST.Node
+, Sibling extends AST.Node | null = AST.Node | null
+, Parent extends AST.Node | null = AST.Node | null
+> =
+  Node extends AST.Root
+  ? Mixin<Node, {
+      parent: null
+      previousSibling: null
+      nextSibling: null
+      children: Array<WrappedNode<AST.BlockContent, AST.BlockContent, AST.Root>>
+    }>
+: Node extends AST.Paragraph
+  ? Mixin<Node, {
+      parent: Parent
+      previousSibling: Sibling
+      nextSibling: Sibling
+      children: Array<WrappedNode<AST.InlineContent, AST.InlineContent, AST.Paragraph>>
+    }>
+: Node extends AST.Heading
+  ? Mixin<Node, {
+      parent: Parent
+      previousSibling: Sibling
+      nextSibling: Sibling
+      children: Array<WrappedNode<AST.InlineContent, AST.InlineContent, AST.Heading>>
+    }>
+: Node extends AST.Blockquote
+  ? Mixin<Node, {
+      parent: Parent
+      previousSibling: Sibling
+      nextSibling: Sibling
+      children: Array<WrappedNode<AST.BlockContent, AST.BlockContent, AST.Blockquote>>
+    }>
+: Node extends AST.List
+  ? Mixin<Node, {
+      parent: Parent
+      previousSibling: Sibling
+      nextSibling: Sibling
+      children: Array<WrappedNode<AST.ListItem, AST.ListItem, AST.List>>
+    }>
+: Node extends AST.ListItem
+  ? Mixin<Node, {
+      parent: Parent
+      previousSibling: Sibling
+      nextSibling: Sibling
+      children: Array<WrappedNode<AST.BlockContent, AST.BlockContent, AST.ListItem>>
+    }>
+: Node extends AST.Emphasis
+  ? Mixin<Node, {
+      parent: Parent
+      previousSibling: Sibling
+      nextSibling: Sibling
+      children: Array<WrappedNode<AST.InlineContent, AST.InlineContent, AST.Emphasis>>
+    }>
+: Node extends AST.Strong
+  ? Mixin<Node, {
+      parent: Parent
+      previousSibling: Sibling
+      nextSibling: Sibling
+      children: Array<WrappedNode<AST.InlineContent, AST.InlineContent, AST.Strong>>
+    }>
+: Node extends AST.Link
+  ? Mixin<Node, {
+      parent: Parent
+      previousSibling: Sibling
+      nextSibling: Sibling
+      children: Array<WrappedNode<AST.InlineContent, AST.InlineContent, AST.Link>>
+    }>
+: Node extends AST.Table
+  ? Mixin<Node, {
+      parent: Parent
+      previousSibling: Sibling
+      nextSibling: Sibling
+      header: WrappedNode<AST.TableRow, null, AST.Table>
+      children: Array<WrappedNode<AST.TableRow, AST.TableRow, AST.Table>>
+    }>
+: Node extends AST.TableRow
+  ? Mixin<Node, {
+      parent: Parent
+      previousSibling: Sibling
+      nextSibling: Sibling
+      children: Array<WrappedNode<AST.TableCell, AST.TableCell, AST.TableRow>>
+    }>
+: Node extends AST.TableCell
+  ? Mixin<Node, {
+      parent: Parent
+      previousSibling: Sibling
+      nextSibling: Sibling
+      children: Array<WrappedNode<AST.InlineContent, AST.InlineContent, AST.TableCell>>
+    }>
+: Node extends AST.Delete
+  ? Mixin<Node, {
+      parent: Parent
+      previousSibling: Sibling
+      nextSibling: Sibling
+      children: Array<WrappedNode<AST.InlineContent, AST.InlineContent, AST.Delete>>
+    }>
+: Node extends AST.Footnote
+  ? Mixin<Node, {
+      parent: Parent
+      previousSibling: Sibling
+      nextSibling: Sibling
+      children: Array<
+        WrappedNode<AST.BlockContent, AST.BlockContent, AST.Footnote>
+      >
+    }>
+: Node extends AST.InlineFootnote
+  ? Mixin<Node, {
+      parent: Parent
+      previousSibling: Sibling
+      nextSibling: Sibling
+      children: Array<
+        WrappedNode<AST.InlineContent, AST.InlineContent, AST.InlineFootnote>
+      >
+    }>
+: Node extends AST.TextDirective
+  ? Mixin<Node, {
+      parent: Parent
+      previousSibling: Sibling
+      nextSibling: Sibling
+      children: Array<
+        WrappedNode<AST.InlineContent, AST.InlineContent, AST.TextDirective>
+      >
+    }>
+: Node extends AST.LeafDirective
+  ? Mixin<Node, {
+      parent: Parent
+      previousSibling: Sibling
+      nextSibling: Sibling
+      children: Array<
+        WrappedNode<AST.InlineContent, AST.InlineContent, AST.LeafDirective>
+      >
+    }>
+: Node extends AST.ContainerDirective
+  ? Mixin<Node, {
+      parent: Parent
+      previousSibling: Sibling
+      nextSibling: Sibling
+      children: Array<
+        WrappedNode<AST.BlockContent, AST.BlockContent, AST.ContainerDirective>
+      >
+    }>
+: Node extends AST.Gallery
+  ? Mixin<Node, {
+      parent: AST.Root
+      previousSibling: null
+      nextSibling: null
+      children: Array<WrappedNode<AST.Image, AST.Image, AST.Gallery>>
+    }>
+: Mixin<Node, {
+    parent: Parent
+    previousSibling: Sibling
+    nextSibling: Sibling
+  }>
+
+export function wrapAST(root: AST.Root): WrappedNode<AST.Root> {
+  const newRoot = cloneDeep(root)
+  wrapNode(newRoot)
+  return newRoot as WrappedNode<AST.Root>
+}
+
+function wrapNode<Node extends AST.Node, Parent extends AST.Node & AST.Parent>(
+  node: Node
+, parent?: Parent
+, index?: number
+): void {
+  const wrappedNode = node as WrappedNode<Node>
+  wrappedNode.parent = null
+  wrappedNode.previousSibling = null
+  wrappedNode.nextSibling = null
+
+  if (isntUndefined(parent)) {
+    wrappedNode.parent = parent
+
+    if (isntUndefined(index)) {
+      wrappedNode.previousSibling = parent.children[index - 1] ?? null
+      wrappedNode.nextSibling = parent.children[index + 1] ?? null
+    }
+  }
+
+  if (isParent(wrappedNode)) {
+    wrapChildren(wrappedNode)
+  }
+
+  if (isTable(wrappedNode)) {
+    wrapNode(wrappedNode.header, wrappedNode)
+  }
+}
+
+function wrapChildren<Parent extends AST.Node & AST.Parent>(parent: Parent): void { 
+  parent.children.forEach((node, i) => wrapNode(node, parent, i))
+}
