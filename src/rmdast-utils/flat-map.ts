03d40e4b3f6b0f92e07dc84b1f82886f7ba0b4c8
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-08-11 16:28:56 +0000

    docs: add comments

--- src/rmdast-utils/flat-map.ts
+++ src/rmdast-utils/flat-map.ts
@@ -2,6 +2,8 @@ import * as RMDAST from './rmdast-2.0'
 import { isParent } from './is'
 import { produce, original } from 'immer'
 
+// 无法用cloneDeep替换immer, 因为immer能够在调用fn时将原始的node作为参数传给调用者,
+// 该特性对于像splitTitleBody这样的调用者特别有用, 因为这些调用者对节点的判断是基于引用相等性的.
 export function flatMap(
   node: RMDAST.Node
 , fn: (node: RMDAST.Node) => RMDAST.Node[]
